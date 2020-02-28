11bde8a9907ee72eaf22bb4017d8dba9475ca2d9
Michele Rawlins <shellbop@att.net>
2020-02-27 22:17:21 -0600

    waited too long

--- javascripts/components/bear.js
+++ javascripts/components/bear.js
@@ -1,19 +1,22 @@
- import bearData from './bearData.js';
+ import getBears from './bearData.js';
+ import cardBears from './river.js';
+  import river from './river.js';
  
 
  const addBear = (e) => {
      console.log("addbear");
-     const bearName = document.getElementById('bear-name').value;
-     const bearImage = document.getElementById('bear-image').value;
-    // const bearId = e.target.id;
-    bearData.setBear(bearId);
-    
+    //  const bearName = document.getElementById('bear-name').value;
+    //  const bearImage = document.getElementById('bear-image').value;
+     const bearId = e.target.id;
+    //  getBears.allBears({name:bearName,image: bearImage});
+    //  river.cardBears();
+    //  document.getElementById('new-bear-form').reset();
   };
-
-  const addBearEvent = (e) => {
+ 
+  const addBearEvent = () => {
     document.getElementsById('submit-bear').addEventListener('click', addBear);
     };
 
 
 
-export default { addBearEvent };
+export default { addBearEvent,addBear };
--- main.js
+++ main.js
@@ -1,8 +1,12 @@
-import bearEvent from '../bears.js';
+import bearEvent from './javascripts/components/bear.js';
+import printToDom from './javascripts/helpers/data/util.js';
+import cardBears from './javascripts/components/river.js';
+import bearData from './javascripts/helpers/data/bearData.js';
 
 const init = () =>{
 
-addBearEvent();
+bear.addBearEvent();
+bearData.getBears();
 
 };
 
